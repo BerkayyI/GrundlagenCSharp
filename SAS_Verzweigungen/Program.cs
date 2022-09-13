5cbcd374ecc4b3c629725e5a2412f63e73c7ec08
poend <poend@YOOMAGWAZZUP>
2022-09-13 22:36:55 +0200

    Alle Aufgaben erledigt

--- SAS_Verzweigungen/Program.cs
+++ SAS_Verzweigungen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
 
 class Aufgaben_SAS_Verzweigungen
 {
@@ -6,7 +8,7 @@ class Aufgaben_SAS_Verzweigungen
     {
         while (true)
         {
-            Console.WriteLine("Which Aufgabe you wanna see ?\nOptions [1],[2],[4]");
+            Console.WriteLine("Welche Aufgabe wollen sie sehen ?\nOptions [1],[2],[3],[4]");
             string Answer = Console.ReadLine();
             if (Answer == "1")
             {
@@ -15,6 +17,7 @@ class Aufgaben_SAS_Verzweigungen
                 Console.Clear();
                 continue;
             }
+
             if (Answer == "2")
             {
                 Console.Clear();
@@ -22,6 +25,15 @@ class Aufgaben_SAS_Verzweigungen
                 Console.Clear();
                 continue;
             }
+
+            if (Answer == "3")
+            {
+                Console.Clear();
+                Aufgabe3();
+                Console.Clear();
+                continue;
+            }
+
             if (Answer == "4")
             {
                 Console.Clear();
@@ -30,6 +42,7 @@ class Aufgaben_SAS_Verzweigungen
                 continue;
 
             }
+
             else
             {
                 return;
@@ -43,7 +56,7 @@ class Aufgaben_SAS_Verzweigungen
         Random r = new Random();
         int LowNum = r.Next(1, 5);
         int HighNum = r.Next(5, 10);
-        Console.WriteLine("Which Number is the smallest {0} or {1}", LowNum, HighNum);
+        Console.WriteLine("Welche Nummer ist die kleinste {0} oder {1}", LowNum, HighNum);
         string answer = Console.ReadLine();
         int ConvertAnswerToInt = Convert.ToInt32(answer);
         if (ConvertAnswerToInt == LowNum)
@@ -52,8 +65,9 @@ class Aufgaben_SAS_Verzweigungen
         }
         else
         {
-            Console.WriteLine("False good luck next time it was {0}", LowNum);
+            Console.WriteLine("Falsch glück beim nächsten Mal es war {0}", LowNum);
         }
+        Thread.Sleep(2500);
 
     }
 
@@ -61,7 +75,7 @@ class Aufgaben_SAS_Verzweigungen
     {
         Random r = new Random();
         int SpecialNum = r.Next(0, 1000);
-        Console.WriteLine("Guess the number between 0-1000");
+        Console.WriteLine("Rate die Zahl zwischen 0-1000");
 
         while (true)
         {
@@ -70,64 +84,94 @@ class Aufgaben_SAS_Verzweigungen
 
             if (ConvertAnswerToInt == SpecialNum)
             {
-                Console.WriteLine("You won");
+                Console.WriteLine("Du hast gewonnen!");
                 break;
 
             }
             if (ConvertAnswerToInt > SpecialNum)
             {
 
-                Console.WriteLine("the Number is lower");
+                Console.WriteLine("die Zahl ist niedriger als {0}", ConvertAnswerToInt);
 
             }
             if (ConvertAnswerToInt < SpecialNum)
             {
 
-                Console.WriteLine("the Number is Higher");
+                Console.WriteLine("die Zahl ist höher als {0}", ConvertAnswerToInt);
 
             }
         }
+        Thread.Sleep(3500);
 
     }
 
+    static void Aufgabe3()
+    {
+        Console.WriteLine("Unser spezieller Herzinfarkt-Risikotest!");
+        Console.WriteLine("Sind Sie übergewichtig?");
+        string OverWeight = Console.ReadLine();
+        Console.WriteLine("Haben sie stress?");
+        string HasStress = Console.ReadLine();
+        if (OverWeight.ToLower() == "ja" && HasStress.ToLower() == "ja")
+        {
+            Console.WriteLine("Sie haben eine Wahrscheinlichkeit von 62 %, einen Herzinfarkt zu erleiden.");
+        }
+        if (OverWeight.ToLower() == "ja" && HasStress.ToLower() == "nein")
+        {
+            Console.WriteLine("Sie haben eine Wahrscheinlichkeit von 18 %, einen Herzinfarkt zu erleiden.");
+        }
+        if (OverWeight.ToLower() == "nein" && HasStress.ToLower() == "ja")
+        {
+            Console.WriteLine("Sie haben eine Wahrscheinlichkeit von 15 %, einen Herzinfarkt zu erleiden.");
+        }
+        if (OverWeight.ToLower() == "nein" && HasStress.ToLower() == "nein")
+        {
+            Console.WriteLine("Sie haben eine Wahrscheinlichkeit von 5 %, einen Herzinfarkt zu erleiden.");
+        }
+
+        Thread.Sleep(3500);
+    }
+
+
+
     static void Aufgabe4()
     {
-        Console.WriteLine("Input your first number");
+        Console.WriteLine("Geben Sie Ihre erste Zahl ein");
         string N1 = Console.ReadLine();
-        Console.WriteLine("Input your second number");
+        Console.WriteLine("Geben Sie Ihre zweite Zahl ein");
         string N2 = Console.ReadLine();
-        short ConvertN1 = Convert.ToInt16(N1);
-        short ConvertN2 = Convert.ToInt16(N2);
-        Console.WriteLine("What would you like to do?\n1 Addition\n2 Subtraktion\n3 Multiplikation\n4 Division\n5 Potenz\n ");
+        double ConvertN1 = Convert.ToDouble(N1);
+        double ConvertN2 = Convert.ToDouble(N2);
+        Console.WriteLine("Was möchten sie machen?\n1 Addition\n2 Subtraktion\n3 Multiplikation\n4 Division\n5 Potenz\n ");
         string Answer = Console.ReadLine();
         Console.WriteLine("Ihre Auswahl: {0}", Answer);
-        short ConvertAnswer = Convert.ToInt16(Answer);
+        double ConvertAnswer = Convert.ToDouble(Answer);
 
         switch (ConvertAnswer)
         {
 
             case 1:
-                var CalcAddition = ConvertN1 + ConvertN2;
+                double CalcAddition = (double)ConvertN1 + ConvertN2;
                 Console.WriteLine("Ihr Ergebnis ist: {0}", CalcAddition);
                 break;
 
             case 2:
-                var CalcSubtraktion = ConvertN1 - ConvertN2;
+                double CalcSubtraktion = (double)ConvertN1 - ConvertN2;
                 Console.WriteLine("Ihr Ergebnis ist: {0}", CalcSubtraktion);
                 break;
 
             case 3:
-                var CalcMultiplikation = ConvertN1 * ConvertN2;
+                double CalcMultiplikation = (double)ConvertN1 * ConvertN2;
                 Console.WriteLine("Ihr Ergebnis ist: {0}", CalcMultiplikation);
                 break;
 
             case 4:
-                var CalcDivision = ConvertN1 / ConvertN2;
+                double CalcDivision = (double)ConvertN1 / ConvertN2;
                 Console.WriteLine("Ihr Ergebnis ist: {0}", CalcDivision);
                 break;
 
             case 5:
-                var CalcPotenz = Math.Pow(ConvertN1, ConvertN2);
+                double CalcPotenz = Math.Pow(ConvertN1, ConvertN2);
                 Console.WriteLine("Ihr Ergebnis ist: {0}", CalcPotenz);
                 break;
 
@@ -137,9 +181,10 @@ class Aufgaben_SAS_Verzweigungen
 
         }
 
-        Console.WriteLine("Wanna calculate again?\n");
+
+        Console.WriteLine("Wollen sie nochmal rechnen?\n");
         string Ans = Console.ReadLine();
-        if (Ans == "yes")
+        if (Ans.ToLower() == "ja")
         {
             Console.Clear();
             Aufgabe4();
@@ -148,6 +193,8 @@ class Aufgaben_SAS_Verzweigungen
         {
             return;
         }
+
+
 
     }
 }
